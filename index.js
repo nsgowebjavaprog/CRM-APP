871e335fe2aa63073567a378d15bf642856b783c
ns loni <nsloni@gmail.com>
2025-11-11 00:38:38 +0530

    added

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// 1. Logic for Express Server
-
-// 2. Connection b/w DB and Logic
-
-// 3. Route Layers
-
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
-app.listen()
+
+const PORT = process.env.PORT || 5000
+console.log(process.env.PORT);
+app.listen(PORT, () =>{
+    console.log(`Server Was Started @ Port Number: ${PORT}`)
+});
+
+// 1. Logic for Express Server
+// 2. Connection b/w DB and Logic
+// 3. Route Layers
