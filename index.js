0013a025071f7388b029f9a48ea7d4e0c842e4f6
ns loni <nsloni@gmail.com>
2025-11-11 00:30:47 +0530

    userSchema create DB

---
+++ index.js
@@ -0,0 +1,10 @@
+// 1. Logic for Express Server
+
+// 2. Connection b/w DB and Logic
+
+// 3. Route Layers
+
+const express = require("express");
+const app = express();
+
+app.listen()
---
+++ models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true 
+    },
+    userId :{
+        type : String,
+        required : true,
+        unique : true
+    },
+    password : {
+        type : String,
+        required : true,
+        minLength : 8
+    },
+    email : {
+        type : String,
+        required : true,
+        unique : true,
+        lowercase : true,
+        minLength : 10
+    },
+    userType : {
+        type : String,
+        enum : ["CUSTOMER", "ADMIN", "ENGINEER"],
+        required : true,
+        default : "CUSTOMER"
+    },
+    userStatus : {
+        type : String,
+        enum : ["APPROVED", "PENDING", "BLOCKED"],
+        required : true,
+        default : "APPROVED"
+    }
+}, {timestamps : true});
+
+
+module.exports = mongoose.model("User", userSchema);
